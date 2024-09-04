4259ee7f0ae0a8fd73805542fc03d0624924f88f
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-09-04 11:40:00 +0500

    product schema

---
+++ backend/routes/product.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getAllProducts } from '../controllers/product.controller.js';
+
+
+const router = express.Router();
+
+router.get('/',getAllProducts);
+
+
+
+
+export default router;
